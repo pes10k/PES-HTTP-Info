973fa08b8c1e06ca7395eb4ed67f0ab39c3df15b
Peter Snyder <snyderp@gmail.com>
2012-07-28 16:41:28 -0500

    Initial useful code commit
    Initial commit for request and response classes

---
+++ PES/HTTP/Info.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace \PES\HTTP;
+
+/**
+ * PES\HTTP\Info
+ *
+ * This package for requesting information about a file served from a webserver
+ * without having to fetch the entire file/document.  So if you're looking
+ * to find out how large the file hosted at http://example.org/file.zip
+ * is, this class wraps a technique that allows you to findout that it
+ * is 1.5GB, w/o having to download 1.5GB of data first.
+ *
+ * @category    Net
+ * @package     PES\HTTP\Info
+ * @author      Peter Snyder <snyderp@gmail.com>
+ * @version 	1.0
+ */
+
+ /**
+  * Instances of this class handle fetching the request headers for a specific
+  * URL.  This class is the public facing part of this package, and clients
+  * should only need to instantiate instances of this class.  All other
+  * classes in this package are managed automatically.
+  */
+class Info {
+
+	/**
+	 * The URL to request information about.  Should be a fully formed
+	 * url, such as http://example.com/example.jpg
+	 *
+	 * @var string
+	 */
+	protected $url;
+
+	/**
+	 * Constructor optionally allows for setting the URL to the checked
+	 * at instantiation.
+	 *
+	 * @param string $url
+	 *   The URL to ask for information about
+	 */
+	public function __construct($url = FALSE)
+	{
+		if ($url) {
+			$this->setUrl($url);
+		}
+	}
+
+	/**
+	 * Attempts to fetch information about the given URL by requesting only the
+	 * HTTP headers from the server.
+	 *
+	 * @return \PES\HTTP\Info\Response|FALSE
+	 *   If no headers were able to be fetched, returns FALSE.  Otherwise,
+	 *   returns a response object representing the returned header information.	 
+	 */
+	public function fetch()
+	{
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, $this->url());
+		curl_setopt($curl, CURLOPT_HEADER, TRUE);
+		curl_setopt($curl, CURLOPT_NOBODY, TRUE);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+		$headers = curl_exec($curl);
+		curl_close($curl);
+
+		// If we weren't able to get any valid headers from the server,
+		// there is nothing further we can do.
+		if ( ! $headers) {
+
+			return FALSE;
+
+		} else {
+
+			return new \PES\HTTP\Info\Response($headers);
+
+		}
+	}
+
+	/* ******************** */
+	/* ! Getters / Setters  */
+	/* ******************** */
+
+	/**
+	 * Returns the URL that has been set to request from
+	 *
+	 * @return string|FALSE
+	 *   Returns FALSE if no URL has yet been set.  Otherwise, the
+	 *   url as a string
+	 */
+	public function url()
+	{
+		return $this->url;
+	}
+
+	/**
+	 * Sets the URL to request information from.  This should be a full URL,
+	 * including protocol and domain information, in the form of
+	 * http://example.org/example.jpg
+	 *
+	 * @param string $a_url
+	 *   The URL to request information from
+	 *
+	 * @return \PES\HTTP\Info
+	 *   A reference to the current instance, to allow for method chaining
+	 */
+	public function setUrl($a_url)
+	{
+		$this->url = $a_url;
+		return $this;
+	}
+}
---
+++ PES/HTTP/Info/Response.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace \PES\HTTP\Info;
+
+/**
+ * PES\HTTP\Info
+ *
+ * This package for requesting information about a file served from a webserver
+ * without having to fetch the entire file/document.  So if you're looking
+ * to find out how large the file hosted at http://example.org/file.zip
+ * is, this class wraps a technique that allows you to findout that it
+ * is 1.5GB, w/o having to download 1.5GB of data first.
+ *
+ * @category    Net
+ * @package     PES\HTTP\Info
+ * @author      Peter Snyder <snyderp@gmail.com>
+ * @version 	1.0
+ */
+
+/**
+ * Instances of this class wrap information about files and resources served
+ * from webservers over HTTP.  Instances are created and managed by instances
+ * of \PES\HTTP\Info;  Clients should not need to create instances of this
+ * class directly.
+ */
+class Response {
+
+	/**
+	 * The raw HTTP header returned from the webserver for this request.
+	 *
+	 * @var string
+	 */
+	private $headers;
+
+	/**
+	 * The size of the resource, as described by the response header.
+	 *
+	 * @var int|NULL
+	 */
+	private $file_size;
+
+	/**
+	 * The MIMEType of the resource, as advertised by the web server
+	 * in the response header
+	 *
+	 * @var string|NULL
+	 */
+	private $mime_type;
+
+	/**
+	 * The name file being requested, as it exists on the server.  This
+	 * may be different that the file as it appears in the request.  For
+	 * example, if you request http://example.org/foo.zip, the web server
+	 * could actually be fulfilling that request with a file called bar.zip.
+	 * This property would store "bar.zip" in the above case.
+	 *
+	 * @var string|NULL
+	 */
+	private $file_name;
+
+	/**
+	 * The right most extension of the file used to satisfy the HTTP request.
+	 * For example, if you request http://example.org/foo.zip, the web server
+	 * could actually be fulfilling that request with a file called bar.zip.
+	 * This property would store "zip" in the above case.
+	 *
+	 * @param string|NULL
+	 */
+	private $file_extension;
+
+	/**
+	 * Instances of this class must be initilized with the raw header text of
+	 * the response being wrapped.
+	 *
+	 * @param string $headers
+	 *   The response headers from the HTTP response being wrapped
+	 */
+	public function __construct($headers)
+	{
+		$this->setHeaders($headers);
+	}
+
+	/* ******************* */
+	/* ! Getter / Setters  */
+	/* ******************* */
+
+	/**
+	 * Returns the HTTP headers for this request.  This is the raw string
+	 * returned by the server.
+	 *
+	 * @return string
+	 *   The HTTP response headers
+	 */
+	public function headers()
+	{
+		return $this->headers;
+	}
+
+	/**
+	 * Returns the MIMEType of the file, as advertised in the HTTP response
+	 * headers.
+	 *
+	 * @return string|NULL
+	 *   Either the mime type, in string form, of the resource, or NULL
+	 *   if the server didn't advertise a mime type / content type
+	 *   for the resource
+	 */
+	public function mimeType()
+	{
+		return $this->mime_type;
+	}
+
+	/**
+	 * The name file being requested, as it exists on the server.  This
+	 * may be different that the file as it appears in the request.  For
+	 * example, if you request http://example.org/foo.zip, the web server
+	 * could actually be fulfilling that request with a file called bar.zip.
+	 * This property would store "bar.zip" in the above case.
+	 *
+	 * @return string|NULL
+	 *   The name of the file used to satisfy the request, if available.
+	 *   Otherwise, NULL
+	 */
+	public function fileName()
+	{
+		return $this->file_name;
+	}
+
+	/**
+	 * The right most extension of the file used to satisfy the HTTP request.
+	 * For example, if you request http://example.org/foo.zip, the web server
+	 * could actually be fulfilling that request with a file called bar.zip.
+	 * This property would store "zip" in the above case.
+	 *
+	 * @return string|NULL
+	 *   The right most file extension of the served file, if it exists.
+	 *   Otherwise, NULL
+	 */
+	public function fileExtension()
+	{
+		return $this->file_extension;
+	}
+
+	/**
+	 * Sets the HTTP response headers being represented by this class.   This
+	 * method also handles parsing the headers to build out the useful
+	 * information clients may be interested in (filesize, etc.).
+	 *
+	 * @param string $headers
+	 *   The HTTP response headers
+	 *
+	 * @return \PES\HTTP\Info\Response
+	 *   Returns a reference to the current object, to allow for method chaining
+	 */
+	protected function setHeaders($headers)
+	{
+		$this->headers = $headers;
+
+		// Now we can try to extract various useful pieces of information
+		// from the headers via regexes.
+		$file_type_pattern = '/Content-Type:\s?([^\s]+)/i';
+		$file_type_matches = array();
+		if (preg_match($file_type_pattern, $this->headers, $file_type_matches)) {
+			$this->mime_type = trim($file_type_matches[1]);
+		}
+
+		$file_name_pattern = '/filename=([^\s]+)/i';
+		$file_name_matches = array();
+		if (preg_match($file_name_pattern, $this->headers, $file_name_matches)) {
+			$this->file_name = trim($file_name_matches[1]);
+		
+			if (($index = strripos($this->file_name, '.')) !== FALSE) {
+				$this->file_extension = substr($results['filename'], $index + 1);
+			}
+		}
+
+		return $this;
+	}
+}
